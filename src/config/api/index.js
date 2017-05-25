c56526b2f6f562f141281549f3918fe9a6c93815
Andres Roget <andresroget@gmail.com>
2017-05-25 06:51:23 -0400

    update api

--- src/config/api/index.js
+++ src/config/api/index.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://127.0.0.1:5000';
+// const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'https://ar-flask-posts-api.herokuapp.com';
 
 const API_RESOURCES = {
   'TAG': 'tag',
